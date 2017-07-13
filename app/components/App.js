c07e3af5e36bea2e00469d989ee02256012f1e02
Kristine Erickson <ericksonk09@gmail.com>
2017-07-13 12:50:44 -0400

    Install react-router-dom, add navbar and set up routes to remaining components

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,30 @@
 var React = require('react');
 var Popular = require('./Popular');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Nav = require('./Nav');
+var Home = require('./Home');
+var Battle = require('./Battle');
+var Switch = require('react-router-dom').Switch;
 
 class App extends React.Component {
   render() {
     return (
-      <div className="container">
-        <Popular />
-      </div>
+      <Router>
+        <div className="container">
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route exact path="/battle" component={Battle}></Route>
+            <Route path="/popular" component={Popular}></Route>
+            <Route render={() => {
+              return <p>Not Found</p>
+            }}
+            />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
